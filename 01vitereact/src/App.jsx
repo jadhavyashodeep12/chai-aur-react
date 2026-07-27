dcdc5ea7c40ec2b480939799eeacbf76ad3d3c8a
Yashodeep <jadhavyashodeep12@gmail.com>
2026-07-27 23:20:51 +0530

    Add React basic and Vite projects

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Chai from "./chai"
+function App() {
+
+  return (
+    <div>
+    <Chai/>
+    <h1> Hello Yashodeeep</h1>
+    <p> only single element cacn be return in function App() so here only one elemnet whisch is "div"
+      and we can add multiple elements insde div
+    </p>
+    </div>
+  )
+}
+export default App
